9ae76f0d4376f7ed01be2df3daf08509b6079584
zed0 <ben@falconers.me.uk>
2014-01-31 19:42:30 +0000

    Changed install.sh to use https://

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 #oh-my-zsh:
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 #homeshick:
-git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 #my dotfiles managed by homeshick:
-git clone git://github.com/zed0/dotfiles.git $HOME/.homesick/repos/dotfiles
+git clone https://github.com/zed0/dotfiles.git $HOME/.homesick/repos/dotfiles
 $HOME/.homesick/repos/homeshick/home/.homeshick link
 
 #vundle (which should install the rest of the vim plugins):
